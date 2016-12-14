cd35c4ddbaa0a5f54bb76cc064e693726ba58489
Jon Leczner <jleczner@udel.edu>
2016-12-14 16:40:21 -0500

    set up beforeEach, renamed file with spec suffix, added spec to specRunner.html

--- test/spec/masterController.ts
+++
@@ -1,5 +0,0 @@
-describe('my first date', () => {
-    it('rubs the lotion on its skin', () => {
-        expect(true).toBeTruthy('or else it gets the hose again');
-    });
-});
---
+++ test/spec/masterControllerSpec.ts
@@ -0,0 +1,24 @@
+import vdog = dogsrus.virtdog;
+
+describe('In the file masterController.ts', () => {
+    describe('the masterController\'s', () => {
+        let sut: vdog.MasterController,
+        $rootScope: ng.IRootScopeService,
+        $controller: ng.IControllerService,
+        masterControllerParams: {
+            $rootScope: ng.IRootScopeService;
+            eventNames: vdog.EventNames;
+        };
+        describe('constructor', () => {
+
+        });
+        describe('throwSomething method', () => {
+
+        });
+        describe('feedTheDog method', () => {
+
+        });
+    });
+    describe('the MasterAction object\'s constructor', () => {
+    });
+});
